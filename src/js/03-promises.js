c6115b0b5a012a8a5ac2dbe296b603d1729bf05c
Weronika2003 <creative.name2003@gmail.com>
2023-07-03 09:30:30 +0200

    11

---
+++ src/js/03-promises.js
@@ -0,0 +1,35 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const modifyForm = document.querySelector('form');
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function usePromise(e) {
+  e.preventDefault();
+  let changeDelay = Number(e.target.delay.value);
+  let changeAmount = Number(e.target.amount.value);
+  let changeStep = Number(e.target.step.value);
+
+  for (let i = 0; i < changeAmount; i += 1) {
+    createPromise(i + 1, changeDelay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    changeDelay += changeStep;
+  }
+}
+modifyForm.addEventListener('submit', usePromise);
